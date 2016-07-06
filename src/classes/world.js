826bd6f445ecf5ed96f6225a33b3e740da1fde74
SpookyCo <rennierab@gmail.com>
2016-07-06 18:37:40 +0100

    bad at git

--- src/classes/world.js
+++ src/classes/world.js
@@ -12,6 +12,59 @@ class World {
     return this.entities[y][x];
   }
 
+  loadMap(cb) {
+    const base_image = new Image();
+    base_image.src = '/assets/map.png';
+
+    const world = this;
+    base_image.onload = function() {
+      const canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d');
+
+      const width = base_image.width;
+      const height = base_image.height;
+      canvas.width = width;
+      canvas.height = height;
+
+      context.drawImage(base_image, 0, 0, width, height);
+
+      const colordata = context.getImageData(0, 0, width, height).data;
+
+      let currentrow = [];
+      for (let i = 0; i < (colordata.length / 4); i++) {
+        const r = colordata[(i * 4)];
+        const g = colordata[(i * 4) + 1];
+        const b = colordata[(i * 4) + 2];
+        const a = colordata[(i * 4) + 3];
+
+        if(i % width == 0) {
+          // A new horizontal row needs to be created.
+          if(i !== 0) {
+            world.map.push(currentrow);
+          }
+          currentrow = [];
+        }
+
+        if(r == 0 && g == 0 && b == 0) {
+          // grass
+          currentrow.push(0);
+        } else {
+          const x = i % width;
+          const y = Math.floor(i / width);
+          console.log(`Map Error: Tile number ${i} (co-ordinates: ${x}, ${y}) cannot be recognized. Color values: ${r}; ${g}; ${b}; ${a}`);
+          alert('Whoops! Something went terribly wrong.');
+          return;
+        }
+
+
+      }
+
+      cb();
+
+    };
+
+  }
+
   randMap() {
     for (let y = 0; y < 99; y++) {
       this.map[y] = [];
