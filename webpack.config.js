6a7da6f3cb26fe375379fad5a037587b425f349d
SpookyCo <rennierab@gmail.com>
2016-07-06 14:43:52 +0100

    webpack n stuff

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+var path = require('path');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry:  {
+    bootstrap: './main.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /pnotify.*\.js$/,
+        loader: 'imports?define=>false,global=>window'
+      },
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader', 'eslint-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.less/,
+        loader: 'style!css!less'
+      },
+      {
+        test: /\.json/,
+        loader: 'json'
+      },
+      {
+        test: /\.png|\.ico|\.xml/,
+        loader:'file-loader?name=favicon/[path][name].[ext]&context=./favicon'
+      },
+      {
+        test: /\.woff|\.woff2|\.svg|.eot|\.ttf/,
+        loader: 'file'
+      },
+      {
+        test: /\.html/,
+        loader: 'html?caseSensitive=true&minimize=false'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+    }
+  },
+  devServer: {
+    port: 9001,
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+  node: {
+    fs: 'empty'
+  },
+  plugins: [
+    new OpenBrowserPlugin({ url: 'http://localhost:9001' })
+  ]
+};
