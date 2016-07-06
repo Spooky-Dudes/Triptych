c7a354bf390e29f3b185e4b2dae92c5abcd62e79
Gytis Daujotas <gytdau@gmail.com>
2016-07-06 16:16:19 +0100

    Added main loadMap() loop

--- src/classes/world.js
+++ src/classes/world.js
@@ -1,19 +1,44 @@
-class Map {
+class World {
   constructor() {
-    this.layout = [[]];
+    this.layout = [
+      []
+    ];
     this.entities = {};
   }
 
-  getTile(x,y) {
+  getTile(x, y) {
     return this.layout[y][x];
   }
 
-  getEntity(x,y) {
+  getEntity(x, y) {
     return this.entities[y][x];
   }
 
   loadMap() {
-    
+    base_image = new Image();
+    base_image.src = 'src/assets/map.png';
+
+    base_image.onload = function() {
+      var canvas = document.createElement("canvas"),
+        context = canvas.getContext('2d');
+
+      var width = base_image.width;
+      var height = base_image.height;
+      canvas.width = width;
+      canvas.height = height;
+
+      context.drawImage(base_image, 0, 0, width, height);
+
+      var colordata = context.getImageData(0, 0, width, height);
+
+      for (var i = 0; i < colordata.length; i = i + 4) {
+        r = colordata[i];
+        g = colordata[i + 1];
+        b = colordata[i + 2];
+
+        // do something
+      }
+    }
   }
 }
 
